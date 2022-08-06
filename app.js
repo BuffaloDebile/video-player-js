61e1f51fb9d691fb76cc443f3005c0f3dc6ff4e2
Paul-Adrien <pauladrienbourgue@gmail.com>
2022-08-06 19:54:24 +0200

    play/pause button + volume slider + progress bar

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const video = document.querySelector('.video');
 const playToggler = document.querySelector('.play-toggler');
 const TogglerImg = document.querySelector('.play-toggler img');
 const timersDisplay = document.querySelectorAll('.time-display');
+const progress = document.querySelector('.progress');
+const muteBtn = document.querySelector('.mute-btn');
+const muteIcon = document.querySelector('.mute-btn img');
+const volumeSlider = document.querySelector('.volume-slider');
+
 let current;
 let totalDuration;
 
@@ -9,6 +14,9 @@ video.addEventListener('click', togglePlay);
 playToggler.addEventListener('click', togglePlay);
 video.addEventListener('loadeddata', fillDurationVariables);
 window.addEventListener('load', fillDurationVariables);
+video.addEventListener('timeupdate', handleTimeUpdate);
+muteBtn.addEventListener('click', handleMute);
+volumeSlider.addEventListener('change', handleVolumeModification);
 
 function togglePlay() {
   if (video.paused) {
@@ -20,14 +28,16 @@ function togglePlay() {
   }
 }
 
-function fillDurationVariables(e) {
-  if (Number.isNan(video.duration)) return;
+function fillDurationVariables() {
+  if (Number.isNaN(video.duration)) return;
 
   current = video.currentTime;
   totalDuration = video.duration;
 
   formatValue(current, timersDisplay[0]);
   formatValue(totalDuration, timersDisplay[1]);
+  video.removeEventListener('loadeddata', fillDurationVariables);
+  window.removeEventListener('load', fillDurationVariables);
 }
 
 function formatValue(val, el) {
@@ -39,4 +49,35 @@ function formatValue(val, el) {
   }
 
   el.textContent = `${currentMin}:${currentSec}`;
+}
+
+function handleTimeUpdate() {
+  current = video.currentTime;
+
+  formatValue(current, timersDisplay[0]);
+
+  const progressPosition = current / totalDuration;
+
+  progress.style.transform = `scaleX(${progressPosition})`;
+
+  if (video.ended) {
+    TogglerImg.src = 'ressources/play.svg';
+  }
+}
+
+function handleMute(e) {
+  if (video.muted) {
+    video.muted = false;
+    muteIcon.src = 'ressources/unmute.svg';
+  } else {
+    video.muted = true;
+    muteIcon.src = 'ressources/mute.svg';
+  }
+}
+
+function handleVolumeModification() {
+  video.volume = volumeSlider.value / 100;
+  if (video.volume === 0) {
+    muteIcon.src = 'ressources/mute.svg';
+  } else muteIcon.src = 'ressources/unmute.svg';
 }
